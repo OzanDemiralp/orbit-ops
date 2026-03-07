6adcf36e7e94e17d55e66cc708e4238110b8066b
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-03-08 01:27:48 +0300

    feat: add cache config and cache to celestrak client service

--- orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/client/CelestrakClient.java
+++ orbit-tracker/src/main/java/com/ozandemiralp/orbit_tracker/client/CelestrakClient.java
@@ -1,5 +1,6 @@
 package com.ozandemiralp.orbit_tracker.client;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -13,6 +14,7 @@ public class CelestrakClient {
         this.webClient = webClient;
     }
 
+    @Cacheable(value = "tleCache")
     public Mono<String> getTleDataByGroup(String group) {
         return this.webClient.get()
                 .uri(uriBuilder -> uriBuilder
